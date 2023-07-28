5fd8d5d8b43b8f699bf38d871a116f43531b4a52
adelina_notebook <isaeva.adelya2016@ya.ru>
2023-07-28 17:11:42 +0300

    task4 is finished

--- src/task4/Task4.java
+++ src/task4/Task4.java
@@ -1,9 +1,33 @@
 package task4;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Task4 {
-    public static void main(String[] args) {
-        FileInputStream file = new FileInputStream(args[0])
+    public static void main(String[] args) throws IOException {
+        FileInputStream file = new FileInputStream(args[0]);
+        String[] text = new String(file.readAllBytes()).split("\\r?\\n");
+        ArrayList<Integer> nums = new ArrayList<>();
+        int sum = 0;
+        for (String one:
+             text) {
+            try {
+                sum += Integer.parseInt(one);
+                nums.add(Integer.parseInt(one));
+            } catch (Exception e){
+            }
+        }
+        int count = 0;
+        for (int num: nums) {
+            if (num != sum / nums.size()){
+                if (num > sum / nums.size()){
+                    count += (num - sum / nums.size());
+                } else {
+                    count += ( - num + sum / nums.size());
+                }
+            }
+        }
+        System.out.print(count);
     }
 }
